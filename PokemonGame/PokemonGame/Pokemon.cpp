e27d71d3e96b9af2e6f74733e536b235ac41e68b
Trey Miller <sloppyjoe06@gmail.com>
2017-12-27 17:48:54 -0800

    Made a defult no arg constructor for pokemon. Made a getter. Defined the basic variables of pokemon.

--- PokemonGame/PokemonGame/Pokemon.cpp
+++ PokemonGame/PokemonGame/Pokemon.cpp
@@ -1,9 +1,25 @@
 #include "stdafx.h"
 #include "Pokemon.h"
-
+using namespace std;
+#include <string>
 
 Pokemon::Pokemon()
 {
+	name = "unknown";
+	attack = 5;
+	defense = 5;
+	evolveLevel = 10;
+	evolveTo = 15;
+	type = "unknown";
+	moves = NULL;
+	curve = 5;
+	levels = NULL;
+	probability = 4;
+}
+
+string Pokemon::getName()
+{
+	return name;
 }
 
 
--- PokemonGame/PokemonGame/Pokemon.h
+++ PokemonGame/PokemonGame/Pokemon.h
@@ -1,8 +1,24 @@
 #pragma once
+using namespace std;
+#include <string>
 class Pokemon
 {
+private:
+
+	string name;
+	int attack;
+	int defense;
+	int evolveLevel;
+	int evolveTo;
+	string type;
+	string* moves;
+	int curve;
+	int* levels;
+	int probability;
+
 public:
 	Pokemon();
+	string getName();
 	~Pokemon();
 };
 

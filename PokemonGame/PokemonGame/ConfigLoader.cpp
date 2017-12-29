a0a65e8fcbe1123d0592edc5d3a1f094eb6ffca4
Trey Miller <sloppyjoe06@gmail.com>
2017-12-29 15:49:41 -0800

    creating pokemon from json data

--- PokemonGame/PokemonGame/ConfigLoader.cpp
+++ PokemonGame/PokemonGame/ConfigLoader.cpp
@@ -1,13 +1,13 @@
 #include "stdafx.h"
 #include "ConfigLoader.h"
 
-#//include "json.hpp"
+#include "json.hpp"
 using namespace std;
 #include <iostream>
 #include <fstream>
 #include <string>
 // for convenience
-//using json = nlohmann::json;
+using json = nlohmann::json;
 
 ConfigLoader::ConfigLoader()
 {
